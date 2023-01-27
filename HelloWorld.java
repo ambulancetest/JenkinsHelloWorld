c5cace614f5815739b115cfcedf0281f43e0c356
ambulancetest <98759161+ambulancetest@users.noreply.github.com>
2023-01-27 12:08:40 +0530

    Update HelloWorld.java

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,5 +1,5 @@
 public class HelloWorld {
 	public static void main(String[] args){
-		System.out.println("-----***** this is working fine****-----");
+		System.out.println("-----***** trying web hook****-----");
 	}
 }
