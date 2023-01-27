6bac54a6fc69b8de60951dae12c3974941e57bf1
ambulancetest <98759161+ambulancetest@users.noreply.github.com>
2023-01-27 12:05:34 +0530

    Update HelloWorld.java
    this is good working

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,5 +1,5 @@
 public class HelloWorld {
 	public static void main(String[] args){
-		System.out.println("-----***** mgggggg****-----");
+		System.out.println("-----***** this is working fine****-----");
 	}
 }
